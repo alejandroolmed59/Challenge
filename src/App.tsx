7d64be1b8d790bda457325824ed841eccd04d36a
alejandroolmed59 <00097017@uca.edu.sv>
2021-08-24 16:07:08 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
+import axios, { AxiosResponse } from "axios";
+import { Table, Button } from "reactstrap";
 
-function App() {
+interface VendingSchema {
+  id: string;
+  name: string;
+  preparation_time: number;
+  thumbnail: string;
+}
+
+const App: React.FC = ()=> {
+  const [vending, setVending] = useState<VendingSchema[]>([]);
+
+  const dispatch = (item : VendingSchema) =>{
+    console.log(item);
+  }
+
+  useEffect(() => {
+    axios
+      .get<VendingSchema[]>("https://vending-machine-test.vercel.app/api/products")
+      .then((response: AxiosResponse) => {
+        setVending(response.data.data);
+        console.log("hola", response.data.data);
+      })
+      .catch((rejected) => {
+        console.log(rejected);
+      });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Table dark responsive>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>name</th>
+              <th>preparation</th>
+              <th>Despachar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {vending.map((item) => {
+              return (
+                <tr>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.preparation_time}</td>
+                  <td><Button onClick={()=> dispatch(item)}>Despachar {item.name}</Button></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
       </header>
     </div>
   );
