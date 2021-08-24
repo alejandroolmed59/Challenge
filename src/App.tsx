02ea0066cb44dee6b9f30c6600a0dd669ea5bbcb
alejandroolmed59 <00097017@uca.edu.sv>
2021-08-24 17:59:34 -0600

    Table w/ queue integrated

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import "./App.css";
 import axios, { AxiosResponse } from "axios";
-import { Table, Button } from "reactstrap";
+import { Table, Button, Badge } from "reactstrap";
 
 interface VendingSchema {
   id: string;
@@ -11,47 +11,80 @@ interface VendingSchema {
   thumbnail: string;
 }
 
-
-const App: React.FC = ()=> {
-  const [dispatchCola , setCola] = useState<VendingSchema[]>([]) 
+const App: React.FC = () => {
+  const [dispatchCola, setCola] = useState<VendingSchema[]>([]);
   const [vending, setVending] = useState<VendingSchema[]>([]);
 
-  const dispatch = (candidato : VendingSchema) =>{
-    if(dispatchCola.some(value=>{
-      return value.id === candidato.id 
-    })){
-      console.log("Ya esta siendo ");
-    }else{
-      setCola((prevState: VendingSchema[])=>{
-        return [...prevState, candidato]
+  const dispatch = (item: VendingSchema) => {
+    const candidato = { ...item };
+    if (
+      dispatchCola.some((value) => {
+        return value.id === candidato.id;
       })
-      const intervalo = setInterval(()=>{
-        candidato.preparation_time-=1
-        setCola((prevState: VendingSchema[])=>{
-          return [...prevState]
-        })
-        if(candidato.preparation_time===0){
-          clearInterval(intervalo)
+    ) {
+      console.log("Ya esta siendo ");
+    } else {
+      setCola((prevState: VendingSchema[]) => {
+        return [...prevState, candidato];
+      });
+      const intervalo = setInterval(() => {
+        candidato.preparation_time -= 1;
+        setCola((prevState: VendingSchema[]) => {
+          return [...prevState];
+        });
+        if (candidato.preparation_time === 0) {
+          clearInterval(intervalo);
         }
-      }, 1000)
+      }, 1000);
     }
-  }
+  };
 
   useEffect(() => {
     axios
-      .get<VendingSchema[]>("https://vending-machine-test.vercel.app/api/products")
+      .get<VendingSchema[]>(
+        "https://vending-machine-test.vercel.app/api/products"
+      )
       .then((response: AxiosResponse) => {
         setVending(response.data.data);
       })
       .catch((rejected) => {
         console.log(rejected);
       });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
+  const TablaQueue = (
+    <>
+      <h1>Items en cola</h1>
+      <Table dark responsive>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>Tiempo restante</th>
+            <th>Estado</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dispatchCola.map((item) => {
+            return (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.preparation_time}</td>
+                <td>
+                  {item.preparation_time===0?<Button color='success' active={false}> DESPACHADO'</Button>:<Button active={false} color='warning'> EN PROCESO</Button>}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </>
+  );
   return (
     <div className="App">
       <header className="App-header">
+        {TablaQueue}
         <Table dark responsive>
           <thead>
             <tr>
@@ -68,7 +101,11 @@ const App: React.FC = ()=> {
                   <td>{item.id}</td>
                   <td>{item.name}</td>
                   <td>{item.preparation_time}</td>
-                  <td><Button onClick={()=> dispatch(item)}>Despachar {item.name}</Button></td>
+                  <td>
+                    <Button onClick={() => dispatch(item)}>
+                      Despachar {item.name}
+                    </Button>
+                  </td>
                 </tr>
               );
             })}
@@ -77,6 +114,6 @@ const App: React.FC = ()=> {
       </header>
     </div>
   );
-}
+};
 
 export default App;
